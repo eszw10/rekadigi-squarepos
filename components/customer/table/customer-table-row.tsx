eb045bd9633681268c03e754553273d0cda2d8c4
eryc_s.z.w <erycszw10@gmail.com>
2025-06-12 19:58:14 +0700

    fix coloring

--- components/customer/table/customer-table-row.tsx
+++ components/customer/table/customer-table-row.tsx
@@ -14,6 +14,19 @@ function levelColor(value: string) {
   }
 }
 
+function levelBg(value: string) {
+  switch (value) {
+    case "Warga":
+      return "bg-secondary-50";
+    case "Juragan":
+      return "bg-ocean-50";
+    case "Sultan":
+      return "bg-leaf-50";
+    case "Konglomerat":
+      return "bg-veronica-50";
+  }
+}
+
 const CustomerTableRow = () => {
   return (
     <tr className="text-dark w-full flex flex-col text-sm">
@@ -23,22 +36,30 @@ const CustomerTableRow = () => {
           className=" grid grid-cols-5 p-2.5 items-center w-full"
         >
           <p className=" w-full p-2.5">{customer.name}</p>
-          <p
-            className={`text-center p-2.5 w-full text-transparent bg-clip-text bg-linear-[135deg] ${levelColor(
-              customer.level
-            )}`}
+          <div
+            className={`py-2 px-6 w-fit rounded-sm ${levelBg(customer.level)}`}
           >
-            {customer.level}
-          </p>
+            <p
+              className={`text-center text-transparent bg-clip-text bg-linear-[135deg] ${levelColor(
+                customer.level
+              )}`}
+            >
+              {customer.level}
+            </p>
+          </div>
           <p className="p-2.5">{customer.favMenu}</p>
           <p className=" p-2.5">IDR {customer.totalTransaction}</p>
-          <div className=" px-2.5 flex items-center gap-4">
-            <div className="flex items-center gap-2 py-2 px-1.5">
+          <div className=" px-2.5 flex items-center gap-4 ">
+            <div className="flex items-center gap-2 py-2 px-4 bg-neutral-50 rounded-sm">
               <ShieldSearch size={12} variant="TwoTone" />
               <span>Detail</span>
             </div>
-            <Edit2 variant="TwoTone" size={12} className="" />
-            <Trash variant="TwoTone" className="text-error-300" size={12} />
+            <div className="py-[8.5px] px-3 bg-neutral-50 rounded-sm">
+              <Edit2 variant="TwoTone" size={12} />
+            </div>
+            <div className="bg-error-100 py-[8.5px] px-3 rounded-sm">
+              <Trash variant="TwoTone" className="text-error-300" size={12} />
+            </div>
           </div>
         </td>
       ))}
--- components/pagination/index.tsx
+++ components/pagination/index.tsx
@@ -4,21 +4,13 @@ const Pagination = () => {
   return (
     <div className="text-sm text-steel flex items-center">
       <div className="flex items-center">
-        <div className="py-[6.5px] px-4 text-dark shadow-[2px_2px_4px_0px_#0000000D]">
+        <div className="py-[6.5px] px-4 bg-white text-dark shadow-[2px_2px_4px_0px_#0000000D]">
           1
         </div>
-        <div className="py-[6.5px] px-4 shadow-[2px_2px_4px_0px_#0000000D]">
-          2
-        </div>
-        <div className="py-[6.5px] px-4 shadow-[2px_2px_4px_0px_#0000000D]">
-          3
-        </div>
-        <div className="py-[6.5px] px-4 shadow-[2px_2px_4px_0px_#0000000D]">
-          ...
-        </div>
-        <div className="py-[6.5px] px-4 shadow-[2px_2px_4px_0px_#0000000D]">
-          38
-        </div>
+        <div className="py-[6.5px] px-4 ">2</div>
+        <div className="py-[6.5px] px-4">3</div>
+        <div className="py-[6.5px] px-4">...</div>
+        <div className="py-[6.5px] px-4">38</div>
       </div>
       <button className=" flex items-center py-2 px-4 gap-3">
         <span>Next</span>
